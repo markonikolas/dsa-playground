f6729e39fe8c38071d242518e2dbda9bc87c3f45
Marko Nikolaš <marko.nikolas@protonmail.com>
2023-06-19 14:43:20 +0200

    Update existing, and add a new test

--- test/LinkedList.test.ts
+++ test/LinkedList.test.ts
@@ -8,33 +8,39 @@ describe('SinglyLinkedList should exist', () => {
 
 	let singlyLinkedList = new SinglyLinkedList();
 
-	test('can be an empty list', async () => expect(singlyLinkedList.head).toBeNull());
+	test('can be an empty list', () => expect(singlyLinkedList.head).toBeNull());
 
-	test('can append a node', async () => {
+	test('can append a node', () => {
 		singlyLinkedList.append(node1);
 
 		expect(singlyLinkedList.head).toStrictEqual(node1);
 	});
 
-	test('can prepend a node', async () => {
+	test('can search for a node by contents', () => {
+		expect(singlyLinkedList.search('new node')).toBe(node1);
+	});
+
+	test('can prepend a node', () => {
 		singlyLinkedList.prepend(node2);
 
 		expect(singlyLinkedList.search('another node')).toEqual(node2);
 	});
 
-	test('can insert a node', async () => {
+	test('can insert a node', () => {
 		singlyLinkedList.insert(node3, node1);
 
 		expect(singlyLinkedList.search('hello world')).toEqual(node3);
 	});
 
-	test('can delete a node', async () => {
+	test('can delete a node', () => {
 		singlyLinkedList.delete(node1, node2);
 
 		expect(singlyLinkedList.search(node1.data)).toBeNull();
 	});
 
-	test('has a HEAD node', async () => {
-		expect(singlyLinkedList.head).toBeDefined();
+	test('has a HEAD node', () => {
+		expect(singlyLinkedList).toHaveProperty('head');
 	});
+
+	test('can traverse the list', () => expect(singlyLinkedList.traverse()).toBeTruthy());
 });

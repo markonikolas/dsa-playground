dc970ef314ce42e55a3ea5f081da9168e4679e40
Marko Nikolaš <marko.nikolas@protonmail.com>
2023-06-19 14:51:41 +0200

    Add tests

--- src/DataStructures/DoublyLinkedList/DoublyLinkedList.ts
+++ src/DataStructures/DoublyLinkedList/DoublyLinkedList.ts
@@ -1,8 +1,42 @@
 import { LinkedList } from '../../Abstract';
+import { ILinkedList } from '../../Interfaces/LinkedList';
+import { ListNodeType } from '../../types';
 import { DoublyListNode } from './DoublyListNode';
 
-export class DoublyLinkedList<T> extends LinkedList<T> {
-    constructor(head: DoublyListNode<T>) {
+export class DoublyLinkedList<T> extends LinkedList<T> implements ILinkedList<T> {
+    constructor(head: DoublyListNode<T> | null = null) {
         super(head);
+    }
+
+    get head(): ListNodeType<T> | null {
+        return this._head;
+    }
+
+    set head(node: ListNodeType<T>) {
+        this._head = node;
+    }
+
+    prepend(node: ListNodeType<T>): ListNodeType<T> {
+        throw new Error('Method not implemented.');
+    }
+
+    append(node: ListNodeType<T>): ListNodeType<T> {
+        throw new Error('Method not implemented.');
+    }
+
+    delete(prev: ListNodeType<T>, node: ListNodeType<T>): void {
+        throw new Error('Method not implemented.');
+    }
+
+    insert(node: ListNodeType<T>, prev: ListNodeType<T>): void {
+        throw new Error('Method not implemented.');
+    }
+
+    traverse(): string {
+        throw new Error('Method not implemented.');
+    }
+
+    search(data: T): ListNodeType<T> {
+        throw new Error('Method not implemented.');
     }
 }
--- test/LinkedList.test.ts
+++ test/LinkedList.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from '@jest/globals';
-import { SinglyLinkedList, SinglyListNode } from '../src/DataStructures';
+import { DoublyLinkedList, DoublyListNode, SinglyLinkedList, SinglyListNode } from '../src/DataStructures';
 
 describe('SinglyLinkedList should exist', () => {
 	const node1 = new SinglyListNode('new node');
@@ -43,4 +43,14 @@ describe('SinglyLinkedList should exist', () => {
 	});
 
 	test('can traverse the list', () => expect(singlyLinkedList.traverse()).toBeTruthy());
+});
+
+describe('DoublyLinkedList should exist', () => {
+	const node1 = new DoublyListNode('node');
+	const node2 = new DoublyListNode([1, 2, 3]);
+	const node3 = new DoublyListNode(10);
+
+	let doublyLinkedList = new DoublyLinkedList();
+
+	test('can be an empty list', () => expect(doublyLinkedList.head).toBeNull());
 });

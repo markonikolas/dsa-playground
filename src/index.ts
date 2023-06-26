24730193a1af8bbc3b2d70e672419c704e40d1f0
Marko Nikolaš <marko.nikolas@protonmail.com>
2023-06-26 16:45:48 +0200

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import { SinglyListNode, SinglyLinkedList } from './DataStructures';
-
-const node = new SinglyListNode<number>(1);
-const anotherNode = new SinglyListNode<number>(2);
-const list = new SinglyLinkedList<number>(node);
-
-const evenMoreNodes = new SinglyListNode<number>(3);
-const lastNode = new SinglyListNode<number>(4);
-
-list.append(anotherNode)
-list.append(lastNode)
-list.prepend(evenMoreNodes)
-list.traverse();
+console.log('Hello World!');
